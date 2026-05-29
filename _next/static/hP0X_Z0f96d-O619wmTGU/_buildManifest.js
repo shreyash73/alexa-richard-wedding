3b75e35fa0f6fd5def1328c67f923be9a606b396
Shreyash Shanbhag <shreyash@Shreyashs-MacBook-Pro.local>
2026-05-30 03:28:15 +0530

    Updates

--- _next/static/hP0X_Z0f96d-O619wmTGU/_buildManifest.js
+++ _next/static/hP0X_Z0f96d-O619wmTGU/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/alexa-richard-wedding//_next/:path+",
+        "destination": "/alexa-richard-wedding/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
