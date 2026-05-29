80587de142e50e5ea6bdcae51db110f1928e077d
Shreyash Shanbhag <shreyash@Shreyashs-MacBook-Pro.local>
2026-05-30 03:23:59 +0530

    Add template6_full.html and utility scripts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
+  basePath: '/alexa-richard-wedding',
+  assetPrefix: '/alexa-richard-wedding/',
   images: {
     unoptimized: true,
   },
